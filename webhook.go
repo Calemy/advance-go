2ec03cfd7a7e5ff89fdbc750ca9e17e8d1eee6b2
Nanoo <45440100+Calemy@users.noreply.github.com>
2025-12-10 14:03:12 -0300

    fix: worker overflow

--- webhook.go
+++ webhook.go
@@ -12,9 +12,6 @@ var webhookQueue = make(chan discordwebhook.Hook)
 var ticker = time.NewTicker(time.Second * 3)
 
 func StartWebhookWorker(link string) {
-	if link == "" {
-		return
-	}
 	go func() {
 		for payload := range webhookQueue {
 			<-ticker.C
@@ -24,11 +21,13 @@ func StartWebhookWorker(link string) {
 }
 
 func SendEmbed(link string, hook discordwebhook.Hook) error {
+	if link == "" {
+		return nil
+	}
 	payload, err := json.Marshal(hook)
 	if err != nil {
 		log.Fatal(err)
 	}
 	err = discordwebhook.ExecuteWebhook(link, payload)
 	return err
-
 }
